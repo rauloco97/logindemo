a6abd9d329c916b2f1470b42725d4859ea545516
Alorotom <alomlitrop@gmail.com>
2020-07-09 17:26:22 -0500

    login session

--- lista.php
+++ lista.php
@@ -3,55 +3,41 @@
 
 require 'Conexion\conexion.php';
 
+session_start();
+$correo = $_SESSION['correo'];
 
 
 
-if(!isset($_SESSION)) 
-{ 
-    session_start(); 
-
-    $correo = $_SESSION['correo'];
 
 
+$sql = "SELECT * FROM usuarios";
 
-    $sql = "SELECT * FROM usuarios";
 
+$resultado = $conexion->query($sql);
 
-    $resultado = $conexion->query($sql);
-    
-    echo "<p>Bienvenido $correo </p>";
-    ?>
-    
-    
-    <table border="solid">
-     <tr>
-      <td>Id</td>
-      <td>Email</td>
-      <td>Contraseña</td>
-     </tr>
-    
-    <?php
-    
-    foreach($resultado as $fila) {
-     echo "<tr>";
-     echo "<td>".$fila['id']."</td>";
-     echo "<td>".$fila['correo']."</td>";
-     echo "<td>".$fila['pass']."</td>";
-     echo "</tr>";
-    }
-    ?>
-    
-    </table>
-    
-    
-    <a href = "Login/logout.php"> Salir </a>
-    <?php
-
-}
-if($_SESSION['login']!=1)
-{
-    header("Location: index.php");
-    exit;
-}
-
+echo "<p>Bienvenido $correo </p>";
 ?>
+
+
+<table border="solid">
+ <tr>
+  <td>Id</td>
+  <td>Email</td>
+  <td>Contraseña</td>
+ </tr>
+
+<?php
+
+foreach($resultado as $fila) {
+ echo "<tr>";
+ echo "<td>".$fila['id']."</td>";
+ echo "<td>".$fila['correo']."</td>";
+ echo "<td>".$fila['pass']."</td>";
+ echo "</tr>";
+}
+?>
+
+</table>
+
+
+<a href = "Login/logout.php"> Salir </a>
